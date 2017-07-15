601e04bf7db00b5a61b1537c1724da4bff2d6e12
MIKADO\risa <g1527066@st.t-kougei.ac.jp>
2017-07-16 02:09:07 +0900

    ターンの管理

---
+++ Assets/Mikado/Script/GameSystem.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSystem : MonoBehaviour
+{
+
+    public enum GameState
+    {
+        Installation,//物を設置するターン
+        Practice,//開業するターン
+        dig,//掘るターン
+    }
+
+    GameState nowState;
+    public GameState NowState
+    {
+        get { return nowState; }
+    }
+
+
+    GameState oldState = GameState.Installation;
+
+    float nowTime;
+    const float digTime = 5f;//5秒ごとに
+    const float practiceTime = 5f;
+
+    //時間がたったらボタンが押せます
+    bool canPush;
+    public bool CanPush
+    {
+        get { return canPush; }
+    }
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        nowTime = 0;
+        nowState = GameState.Installation;
+        oldState = GameState.dig;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (nowState)
+        {
+
+            //指定したボタンをクリックしたら次に進む
+            case GameState.Installation:
+
+                if (nowState != oldState)
+                {
+                    canPush = true;
+
+                }
+
+                break;
+
+
+
+            //一定時間たったら、指定したボタンで掘るターンへ
+            case GameState.Practice:
+
+                if (nowState != oldState)
+                {
+                    canPush = false;
+                }
+                nowTime += Time.deltaTime;
+
+                if (nowTime > practiceTime)
+                {
+                    nowTime = 0;
+                    canPush = true;
+                }
+
+
+                break;
+
+
+            //     //一定時間たったら、指定したボタンで設置ターンへ
+            case GameState.dig:
+
+                if (nowState != oldState)
+                {
+                    canPush = false;
+                    nowTime = 0;
+                }
+
+
+                nowTime += Time.deltaTime;
+                if (nowTime > digTime)
+                {
+                    nowTime = 0;
+                    canPush = true;
+                }
+
+                break;
+
+        }
+        oldState = nowState;
+    }
+
+    public void NextButton()
+    {
+        if (nowState == GameState.dig)//最後の
+            nowState = GameState.Installation;
+        else
+            nowState++;
+    }
+
+
+}
---
+++ Assets/Mikado/Script/NextButton.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NextButton : MonoBehaviour
+{
+
+    [SerializeField]
+    Text tx;
+
+    [SerializeField]
+    GameObject gameSytemObject;
+
+    public void ButtonPush()
+    {
+        if (gameSytemObject.GetComponent<GameSystem>().CanPush)
+        gameSytemObject.GetComponent<GameSystem>().NextButton();
+ 
+
+    }
+
+
+    //後で消します
+    void Update()
+    {
+
+
+        GameSystem.GameState gameState = gameSytemObject.GetComponent<GameSystem>().NowState ;
+
+
+        switch (gameState)
+        {
+
+            case GameSystem.GameState.Installation:
+                tx.text = "Installation";
+
+                break;
+
+
+            case GameSystem.GameState.Practice:
+                tx.text = "Practice";
+
+                break;
+
+
+
+            case GameSystem.GameState.dig:
+                tx.text = "dig";
+
+                break;
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+    }
